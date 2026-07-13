5c08d868156c9f7a7cc71c933d1e731d95488d94
SamriddhSrivastava <srivastavasamriddh01@gmail.com>
2026-07-14 00:09:02 +0530

    Time: 4 ms (62.39%), Space: 44.3 MB (40.77%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Pair
+    {
+    TreeNode node;
+    int v;
+    int l;
+     Pair(TreeNode n,int ver,int lev)
+     {
+        node=n;
+        v=ver;
+        l=lev;
+     }
+    }
+    public List<List<Integer>> verticalTraversal(TreeNode root) 
+    {
+     TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> nodes=new TreeMap<>();
+     Queue<Pair> q=new LinkedList<>();
+     q.offer(new Pair(root,0,0));
+     while(!q.isEmpty())
+     {
+        Pair p=q.poll();
+        TreeNode temp=p.node;
+        int x=p.v;
+        int y=p.l;
+
+        nodes.putIfAbsent(x,new TreeMap<>());
+        nodes.get(x).putIfAbsent(y,new PriorityQueue<>());
+        nodes.get(x).get(y).offer(temp.val);
+
+        if(temp.left!=null)
+        q.offer(new Pair(temp.left,x-1,y+1));
+        if(temp.right!=null)
+        q.offer(new Pair(temp.right,x+1,y+1));
+     }
+     List<List<Integer>> res=new ArrayList<>();
+     for(TreeMap<Integer,PriorityQueue<Integer>> ts:nodes.values())
+     {
+        List<Integer> col=new ArrayList<>();
+        for(PriorityQueue<Integer> p : ts.values())
+        {
+            while(!p.isEmpty())
+                col.add(p.poll());
+        }
+        res.add(col);
+     }
+     return res;
+    }    
+}
