585a6176da3aa36f1ba1692859190f5f9ab87968
SamriddhSrivastava <srivastavasamriddh01@gmail.com>
2026-08-13 18:16:47 +0530

    Time: 771 ms (12.12%), Space: 0B (100%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT s1.score,
+   (
+    SELECT COUNT(DISTINCT s2.score)
+    FROM Scores as s2
+    WHERE s2.score>=s1.score
+   )as `rank`
+FROM Scores as s1
+ORDER BY s1.score DESC;
