e2c9fc572bc6d79cd1286dfcb970c05c4eaec2b9
SamriddhSrivastava <srivastavasamriddh01@gmail.com>
2026-09-23 22:05:54 +0530

    Time: 96 ms (20.41%), Space: 72.7 MB (27.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -27,3 +27,28 @@ class Solution {
         return dp[ind][s]=b1||b2 ? 1 : 0;
     }
 }
+
+
+//method 2 more precise
+/*class Solution {
+    public boolean canPartition(int[] nums) 
+    {
+        int total=0;
+        for(int n : nums) 
+            total+=n;
+
+        if(total%2!=0)
+            return false;
+
+        int target=total/2;
+        boolean[] dp=new boolean[target+1];
+        dp[0]=true;
+
+        for(int num : nums) 
+        {
+            for(int j=target;j>=num;j--)
+                dp[j]=dp[j] || dp[j-num];
+        }
+        return dp[target];
+    }
+}*/
